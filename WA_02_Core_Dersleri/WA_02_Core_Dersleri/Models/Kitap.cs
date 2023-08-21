9daa252278a41629d0ecd5194bd11867dd8c7c5d
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-08-21 14:42:08 +0300

    Models/Kitap.cs is updated.

--- WA_02_Core_Dersleri/WA_02_Core_Dersleri/Models/Kitap.cs
+++ WA_02_Core_Dersleri/WA_02_Core_Dersleri/Models/Kitap.cs
@@ -7,5 +7,8 @@ namespace WA_02_Core_Dersleri.Models
 {
     public class Kitap
     {
+        public int ID { get; set; }
+        public string KitapAd { get; set; }
+        public string Yazar { get; set; }
     }
 }
