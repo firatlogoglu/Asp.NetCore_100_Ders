d252382759a1204ac2e0c6e0efed0c6a049a4d8c
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-08-21 14:38:33 +0300

    Models/Kitap.cs is created.

---
+++ WA_02_Core_Dersleri/WA_02_Core_Dersleri/Models/Kitap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA_02_Core_Dersleri.Models
+{
+    public class Kitap
+    {
+    }
+}
