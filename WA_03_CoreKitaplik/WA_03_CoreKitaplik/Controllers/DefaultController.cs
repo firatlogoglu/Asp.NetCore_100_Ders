6b3bd1310b7c41603cf026008aa818190b301c9c
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-08-22 23:03:48 +0300

    DefaultController is updated.

--- WA_03_CoreKitaplik/WA_03_CoreKitaplik/Controllers/DefaultController.cs
+++ WA_03_CoreKitaplik/WA_03_CoreKitaplik/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WA_03_CoreKitaplik.Models;
 
 namespace WA_03_CoreKitaplik.Controllers
 {
@@ -10,7 +11,16 @@ namespace WA_03_CoreKitaplik.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var ktp = new List<Kitaplar>()
+            {
+                new Kitaplar(){ID=1, KitapAd="Satranç", Yazar="Stefan Zweig"},
+                new Kitaplar(){ID=2, KitapAd="Doktor Ox'un Deneyi", Yazar="Jules Verne"},
+                new Kitaplar(){ID=3, KitapAd="Delifişek", Yazar="Jose Mauro De Vasconcelos"},
+                new Kitaplar(){ID=4, KitapAd="Kırmız Bisiklet", Yazar="Muzaffer İzgü"},
+                new Kitaplar(){ID=5, KitapAd="Toros Canavarı", Yazar="Aziz Nesin"}
+            };
+
+            return View(ktp);
         }
     }
 }
