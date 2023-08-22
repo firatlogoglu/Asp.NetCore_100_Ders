73db309376669247ed8903556810b3c72813d2a4
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-08-22 22:45:00 +0300

    Models/Kitaplar.cs is updated.

--- WA_03_CoreKitaplik/WA_03_CoreKitaplik/Models/Kitaplar.cs
+++ WA_03_CoreKitaplik/WA_03_CoreKitaplik/Models/Kitaplar.cs
@@ -7,5 +7,8 @@ namespace WA_03_CoreKitaplik.Models
 {
     public class Kitaplar
     {
+        public int ID { get; set; }
+        public string KitapAd { get; set; }
+        public string Yazar { get; set; }
     }
 }
