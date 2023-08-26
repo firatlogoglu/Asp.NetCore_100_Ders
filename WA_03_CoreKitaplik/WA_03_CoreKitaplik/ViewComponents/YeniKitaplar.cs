066b79ae8857b253ebe53179d0977f2cb367f0e9
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-08-26 15:53:50 +0300

    ViewComponents/YeniKitaplar.cs is created.

---
+++ WA_03_CoreKitaplik/WA_03_CoreKitaplik/ViewComponents/YeniKitaplar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA_03_CoreKitaplik.ViewComponents
+{
+    public class YeniKitaplar
+    {
+    }
+}
