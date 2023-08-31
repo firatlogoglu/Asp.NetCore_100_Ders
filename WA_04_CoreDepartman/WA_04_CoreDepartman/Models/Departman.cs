b2bbfd5c2e6bc92efea8b0c5d67878cd534894f7
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-08-31 16:31:30 +0300

    Departman.cs is created.

---
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Models/Departman.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA_04_CoreDepartman.Models
+{
+    public class Departman
+    {
+    }
+}
