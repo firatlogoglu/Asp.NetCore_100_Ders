54682c6d92bd6646c1ff84672764a243785f1f2b
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-08-31 16:34:03 +0300

    Departman.cs is updated.

--- WA_04_CoreDepartman/WA_04_CoreDepartman/Models/Departman.cs
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Models/Departman.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,8 @@ namespace WA_04_CoreDepartman.Models
 {
     public class Departman
     {
+        [Key]
+        public int ID { get; set; }
+        public string Ad { get; set; }
     }
 }
