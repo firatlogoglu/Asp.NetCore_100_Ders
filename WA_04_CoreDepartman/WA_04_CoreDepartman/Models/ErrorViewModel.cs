45b72ff70155029f260da3c6e9307b316addec32
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-08-31 15:57:46 +0300

    WA_04_CoreDepartman is created.

---
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WA_04_CoreDepartman.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
