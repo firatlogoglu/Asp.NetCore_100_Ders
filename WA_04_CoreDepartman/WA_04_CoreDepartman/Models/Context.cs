b0771c93acf59c7036a6261466aa61788ad1ea7f
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-01 14:38:22 +0300

    Context.cs is updated.

--- WA_04_CoreDepartman/WA_04_CoreDepartman/Models/Context.cs
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Models/Context.cs
@@ -8,6 +8,11 @@ namespace WA_04_CoreDepartman.Models
 {
     public class Context : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.; database=CorePersonelDB; uid=sa; pwd=123;");
+        }
+
         public DbSet<Departman> Departmen { get; set; }
         public DbSet<Personel> Personels { get; set; }
     }
