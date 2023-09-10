8eecef87f2be8ea8e322a81bf8aa4cdc692c4356
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-10 23:18:27 +0300

    Add-Migration Test1 -Verbose
    Update-Database -Verbose

--- WA_04_CoreDepartman/WA_04_CoreDepartman/Migrations/20230910201606_Test1.Designer.cs
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Migrations/20230910201606_Test1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WA_04_CoreDepartman.Models;
 
 namespace WA_04_CoreDepartman.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230910201606_Test1")]
+    partial class Test1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Migrations/20230910201606_Test1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WA_04_CoreDepartman.Migrations
+{
+    public partial class Test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmanID",
+                table: "Personels",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personels_DepartmanID",
+                table: "Personels",
+                column: "DepartmanID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Personels_Departmen_DepartmanID",
+                table: "Personels",
+                column: "DepartmanID",
+                principalTable: "Departmen",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Personels_Departmen_DepartmanID",
+                table: "Personels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Personels_DepartmanID",
+                table: "Personels");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmanID",
+                table: "Personels");
+        }
+    }
+}
