6ac8e4c0e2a58bd8d395dfda890ffbdfbd4fcd1e
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-10 22:52:35 +0300

    Add-Migration yenisutun -Verbose
    Update-Database -Verbose (NO)
    Add-Migration yenisutun -Verbose (YES)
    Update-Database -Verbose (YES)

---
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Migrations/20230910194832_yenisutun.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WA_04_CoreDepartman.Migrations
+{
+    public partial class yenisutun : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Detay",
+                table: "Departmen",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Detay",
+                table: "Departmen");
+        }
+    }
+}
