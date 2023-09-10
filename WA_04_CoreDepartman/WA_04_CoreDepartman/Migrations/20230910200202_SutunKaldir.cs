e8f41a079d062f368aab471fc0d57052f2ea66db
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-10 23:04:17 +0300

    Add-Migration SutunKaldir -Verbose
    Update-Database -Verbose

---
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Migrations/20230910200202_SutunKaldir.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WA_04_CoreDepartman.Migrations
+{
+    public partial class SutunKaldir : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Detay",
+                table: "Departmen");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Detay",
+                table: "Departmen",
+                nullable: true);
+        }
+    }
+}
