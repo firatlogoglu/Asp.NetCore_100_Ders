ebdafc53fd9db451c87404006a1c04a85731fa75
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-10 22:52:00 +0300

    Models/Departman.cs is updated.

--- WA_04_CoreDepartman/WA_04_CoreDepartman/Models/Departman.cs
+++ WA_04_CoreDepartman/WA_04_CoreDepartman/Models/Departman.cs
@@ -11,5 +11,6 @@ namespace WA_04_CoreDepartman.Models
         [Key]
         public int ID { get; set; }
         public string Ad { get; set; }
+        public string Detay { get; set; }
     }
 }
