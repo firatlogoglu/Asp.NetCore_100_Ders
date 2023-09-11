712dffa24cce60dc22d4dcb641e11e6b3e4d4279
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-11 15:37:13 +0300

    Models/Context.cs is updated.

--- WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Context.cs
+++ WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Context.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WA_05_CoreDepartman_Revize.Models
 {
-    public class Context
+    public class Context : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.; database=CorePersonel_RevizeDB; uid=sa; pwd=123;");
+        }
+
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
     }
 }
