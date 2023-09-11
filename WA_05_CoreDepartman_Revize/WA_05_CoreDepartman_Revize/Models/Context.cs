1e978644e818fcd1da08009bcef3a8801cb90793
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-11 15:19:34 +0300

    Models/Context.cs is created.

---
+++ WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Context.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA_05_CoreDepartman_Revize.Models
+{
+    public class Context
+    {
+    }
+}
