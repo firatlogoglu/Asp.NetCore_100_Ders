864bf8e593ff738e6ae895665fb5901ec7e6689f
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-11 15:24:36 +0300

    Models/Birim.cs is updated.

--- WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Birim.cs
+++ WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Birim.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace WA_05_CoreDepartman_Revize.Models
 {
     public class Birim
     {
+        [Key]
+        public int ID { get; set; }
+        public string Ad { get; set; }
+
+        public IList<Personel> Personels { get; set; }
     }
 }
