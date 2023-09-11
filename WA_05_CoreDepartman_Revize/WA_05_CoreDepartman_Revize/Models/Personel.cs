c173f7521a2c767c207a4dfb4bbcf8c29219eac5
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-11 15:28:33 +0300

    Models/Personel.cs is updated.

--- WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Personel.cs
+++ WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Personel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace WA_05_CoreDepartman_Revize.Models
 {
     public class Personel
     {
+        [Key]
+        public int ID { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Sehir { get; set; }
+
+        public Birim Birim { get; set; }
+        public int BirimID { get; set; }
     }
 }
