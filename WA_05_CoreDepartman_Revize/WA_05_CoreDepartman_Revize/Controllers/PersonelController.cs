903ef77516e9a0d65f5d69906e1f1a7bcc038214
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-13 19:19:29 +0300

    PersonelController.cs is updated.

--- WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Controllers/PersonelController.cs
+++ WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Controllers/PersonelController.cs
@@ -3,14 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WA_05_CoreDepartman_Revize.Models;
 
 namespace WA_05_CoreDepartman_Revize.Controllers
 {
     public class PersonelController : Controller
     {
+        Context c = new Context();
+
         public IActionResult Index()
         {
-            return View();
+            var degerler = c.Personels.ToList();
+            return View(degerler);
         }
     }
 }
