c7fb1f3afdfba94ee6d3037945f266f41573fe15
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-18 00:17:15 +0300

    Add-Migration Deneme1 -Verbose
    Update-Database -Verbose

---
+++ WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Migrations/20230917211558_Deneme1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WA_05_CoreDepartman_Revize.Migrations
+{
+    public partial class Deneme1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    KullaniciAdi = table.Column<string>(type: "Varchar(20)", nullable: true),
+                    Sifre = table.Column<string>(type: "Varchar(10)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+    }
+}
