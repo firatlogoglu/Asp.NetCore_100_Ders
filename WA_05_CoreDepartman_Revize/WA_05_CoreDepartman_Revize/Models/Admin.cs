86672179c46c6218cf99ed5e36be3e7f3e10fe14
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-18 00:10:23 +0300

    Admin.cs is updated.

--- WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Admin.cs
+++ WA_05_CoreDepartman_Revize/WA_05_CoreDepartman_Revize/Models/Admin.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,13 @@ namespace WA_05_CoreDepartman_Revize.Models
 {
     public class Admin
     {
+        [Key]
+        public int ID { get; set; }
+
+        [Column(TypeName = "Varchar(20)")]
+        public string KullaniciAdi { get; set; }
+
+        [Column(TypeName = "Varchar(10)")]
+        public string Sifre { get; set; }
     }
 }
