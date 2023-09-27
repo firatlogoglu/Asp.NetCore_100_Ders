f18a7b19105b9d3a2271e0574dafc503e3b1eb73
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-28 01:13:46 +0300

    Context.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Data/Models/Context.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Data/Models/Context.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WA_06_CoreAndFood.Data.Models
 {
-    public class Context
+    public class Context:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.; database=DBCoreFood; uid=sa; pwd=123;");
+        }
+
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
