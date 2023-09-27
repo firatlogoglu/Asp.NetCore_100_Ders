90d0547da8baf9ae6344914b44b3583e6f6e9b5f
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-28 00:16:53 +0300

    Food.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Data/Models/Food.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Data/Models/Food.cs
@@ -7,5 +7,13 @@ namespace WA_06_CoreAndFood.Data.Models
 {
     public class Food
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public string ThumbNailImageURL { get; set; }
+        public int Stock { get; set; }
     }
 }
