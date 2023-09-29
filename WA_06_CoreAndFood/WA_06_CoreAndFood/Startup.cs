448c70f2cd92575f880390d5971bc2e8508c9b46
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-29 22:53:53 +0300

    Startup.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Startup.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Startup.cs
@@ -15,6 +15,7 @@ namespace WA_06_CoreAndFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,16 @@ namespace WA_06_CoreAndFood
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Category}/{action=Index}/{id?}");
             });
         }
     }
