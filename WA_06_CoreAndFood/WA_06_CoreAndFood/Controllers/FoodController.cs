db58e4d00aad70dd0fe9353ad08b6e2fa55df686
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-29 23:12:54 +0300

    FoodController.cs is created.

---
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Controllers/FoodController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA_06_CoreAndFood.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
