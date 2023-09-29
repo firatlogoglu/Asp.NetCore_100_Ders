e7fdb313e4ba9e543d01096a398a15e7abcf2764
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-29 23:12:04 +0300

    CategoryController.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Controllers/CategoryController.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WA_06_CoreAndFood.Repositories;
 
 namespace WA_06_CoreAndFood.Controllers
 {
@@ -10,7 +11,8 @@ namespace WA_06_CoreAndFood.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            CategoryRepository categoryRepository = new CategoryRepository();
+            return View(categoryRepository.TList());
         }
     }
 }
