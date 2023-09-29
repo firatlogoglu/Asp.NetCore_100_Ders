67c8cbfc0e48ae50b0a8a905af6c6c4a669ee522
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-29 17:32:42 +0300

    CategoryRepository.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Repositories/CategoryRepository.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Repositories/CategoryRepository.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WA_06_CoreAndFood.Data.Models;
 
 namespace WA_06_CoreAndFood.Repositories
 {
     public class CategoryRepository
     {
+        Context c = new Context();
+
+        public List<Category> CategoryList()
+        {
+            return c.Categories.ToList();
+        }
+
+        public void CategoryAdd(Category ct)
+        {
+            c.Categories.Add(ct);
+            c.SaveChanges();
+        }
+
+        public void CategoryDelete(Category ct)
+        {
+            c.Categories.Remove(ct);
+            c.SaveChanges();
+        }
+
+        public void CategoryUpdate(Category ct)
+        {
+            c.Categories.Update(ct);
+            c.SaveChanges();
+        }
+
+        public void GetCategory(int id)
+        {
+            c.Categories.Find(id);
+        }
     }
 }
