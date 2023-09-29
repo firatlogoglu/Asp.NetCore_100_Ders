c890b7c1aaee66c1c7a2dd53a4d397f45a7ff8f6
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-29 18:14:52 +0300

    FoodRepository.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Repositories/FoodRepository.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Repositories/FoodRepository.cs
@@ -8,34 +8,6 @@ namespace WA_06_CoreAndFood.Repositories
 {
     public class FoodRepository
     {
-        Context c = new Context();
 
-        public void FoodAdd(Food f)
-        {
-            c.Foods.Add(f);
-            c.SaveChanges();
-        }
-
-        public void FoodRemove(Food f)
-        {
-            c.Foods.Remove(f);
-            c.SaveChanges();
-        }
-
-        public void FoodUpdate(Food f)
-        {
-            c.Foods.Update(f);
-            c.SaveChanges();
-        }
-
-        public List<Food> FoodList()
-        {
-            return c.Foods.ToList();
-        }
-
-        public void GetFood(int id)
-        {
-            c.Foods.Find(id);
-        }
     }
 }
