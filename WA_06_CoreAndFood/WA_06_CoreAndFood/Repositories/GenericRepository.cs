d29a7b73f9df1dcda03669e6677ac1c44adfa308
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-09-29 18:14:07 +0300

    GenericRepository.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Repositories/GenericRepository.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Repositories/GenericRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WA_06_CoreAndFood.Data.Models;
 
 namespace WA_06_CoreAndFood.Repositories
 {
@@ -9,32 +10,32 @@ namespace WA_06_CoreAndFood.Repositories
     {
         Context c = new Context();
 
-        public List<Category> CategoryList()
+        public List<T> CategoryList()
         {
-            return c.Categories.ToList();
+            return c.Set<T>().ToList();
         }
 
-        public void CategoryAdd(Category ct)
+        public void CategoryAdd(T p)
         {
-            c.Categories.Add(ct);
+            c.Set<T>().Add(p);
             c.SaveChanges();
         }
 
-        public void CategoryDelete(Category ct)
+        public void CategoryDelete(T p)
         {
-            c.Categories.Remove(ct);
+            c.Set<T>().Remove(p);
             c.SaveChanges();
         }
 
-        public void CategoryUpdate(Category ct)
+        public void CategoryUpdate(T p)
         {
-            c.Categories.Update(ct);
+            c.Set<T>().Update(p);
             c.SaveChanges();
         }
 
         public void GetCategory(int id)
         {
-            c.Categories.Find(id);
+            c.Set<T>().Find(id);
         }
     }
 }
