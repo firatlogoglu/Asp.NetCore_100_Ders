f33fb40744c8333cda6d43abad8ec26b03bf6faa
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-01 23:08:13 +0300

    Category.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Data/Models/Category.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Data/Models/Category.cs
@@ -10,8 +10,10 @@ namespace WA_06_CoreAndFood.Data.Models
     {
         public int ID { get; set; }
 
-        [Required(ErrorMessage ="Category Name Not Empty")]
+        [Required(ErrorMessage = "Category Name Not Empty")]
+        [StringLength(20, ErrorMessage = "Please maximum enter 20 characters")]
         public string Name { get; set; }
+
         public string Description { get; set; }
 
         public List<Food> Foods { get; set; }
