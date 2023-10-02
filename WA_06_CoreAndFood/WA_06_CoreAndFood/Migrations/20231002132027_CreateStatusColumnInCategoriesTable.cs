3b7af4be6596771e8fc662f3d971cea904c64791
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-02 16:25:48 +0300

    Add-Migration CreateStatusColumnInCategoriesTable -Verbose
    Update-Database -Verbose

---
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Migrations/20231002132027_CreateStatusColumnInCategoriesTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WA_06_CoreAndFood.Migrations
+{
+    public partial class CreateStatusColumnInCategoriesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Categories",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+        }
+    }
+}
