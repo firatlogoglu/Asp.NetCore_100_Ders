7350ffa4c2ed0771ae0b93bc338cd878253e4b6d
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-06 11:51:46 +0300

    Add-Migration Mig1 -Verbose
    Update-Database -Verbose

---
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Migrations/20231006085009_Mig1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WA_06_CoreAndFood.Migrations
+{
+    public partial class Mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(maxLength: 20, nullable: true),
+                    Password = table.Column<string>(maxLength: 20, nullable: true),
+                    AdminRole = table.Column<string>(maxLength: 1, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+    }
+}
