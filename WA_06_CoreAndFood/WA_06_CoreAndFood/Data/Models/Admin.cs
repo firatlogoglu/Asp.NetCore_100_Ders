9a1771099ae4e3af4288caad1b9f78c622290d42
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-06 11:45:31 +0300

    Admin.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Data/Models/Admin.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Data/Models/Admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace WA_06_CoreAndFood.Data.Models
 {
     public class Admin
     {
+        [Key]
         public int ID { get; set; }
+
+        [StringLength(20)]
         public string UserName { get; set; }
+
+        [StringLength(20)]
         public string Password { get; set; }
+
+        [StringLength(1)]
         public string AdminRole { get; set; }
     }
 }
