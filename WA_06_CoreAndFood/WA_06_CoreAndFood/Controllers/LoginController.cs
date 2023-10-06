d90d48bc68c09ffc5845443d9445d1418d21b39e
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-06 16:47:02 +0300

    LoginController.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Controllers/LoginController.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Controllers/LoginController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using WA_06_CoreAndFood.Data.Models;
+using WA_06_CoreAndFood.Repositories;
 
 namespace WA_06_CoreAndFood.Controllers
 {
@@ -13,6 +17,25 @@ namespace WA_06_CoreAndFood.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public async Task<IActionResult> Index(Admin p)
+        {
+            AdminRepository a = new AdminRepository();
+            var dataValue = a.TList().FirstOrDefault(x => x.UserName == p.UserName && x.Password == p.Password);
+
+            if (dataValue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.UserName)
+                };
+                var userIdentity = new ClaimsIdentity(claims, "Login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Category");
+            }
             return View();
         }
     }
