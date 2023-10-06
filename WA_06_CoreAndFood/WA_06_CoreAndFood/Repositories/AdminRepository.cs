85d6fc585bcfc56483e6aa01ba8963ab8516b6fb
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-06 16:11:41 +0300

    AdminRepository.cs is updated. (Videoda olmayan)

--- WA_06_CoreAndFood/WA_06_CoreAndFood/Repositories/AdminRepository.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/Repositories/AdminRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WA_06_CoreAndFood.Data.Models;
 
 namespace WA_06_CoreAndFood.Repositories
 {
-    public class AdminRepository
+    public class AdminRepository : GenericRepository<Admin>
     {
     }
 }
