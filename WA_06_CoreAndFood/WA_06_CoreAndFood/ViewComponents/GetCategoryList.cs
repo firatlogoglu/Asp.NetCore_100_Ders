5d1278f6b250c33bb6c4a9be4e7e79dd515d1893
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-07 11:54:39 +0300

    GetCategoryList.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/ViewComponents/GetCategoryList.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/ViewComponents/GetCategoryList.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WA_06_CoreAndFood.ViewComponents
 {
-    public class GetCategoryList
+    public class GetCategoryList : ViewComponent
     {
     }
 }
