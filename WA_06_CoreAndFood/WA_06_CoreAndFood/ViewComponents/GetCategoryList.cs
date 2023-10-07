de0dd5d9176220212cd5a951bbb206a0cd13000e
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-07 12:00:12 +0300

    GetCategoryList.cs is updated.

--- WA_06_CoreAndFood/WA_06_CoreAndFood/ViewComponents/GetCategoryList.cs
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/ViewComponents/GetCategoryList.cs
@@ -3,10 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WA_06_CoreAndFood.Repositories;
 
 namespace WA_06_CoreAndFood.ViewComponents
 {
     public class GetCategoryList : ViewComponent
     {
+        public IViewComponentResult Invoke()
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            var categoryList = categoryRepository.TList();
+            return View(categoryList);
+        }
     }
 }
