e73bf98c9cb3806f858051b0ce5d70ce5855ec88
Fırat Loğoğlu <firatlogoglu@gmail.com>
2023-10-09 09:15:02 +0300

    GetFoodList.cs is created.

---
+++ WA_06_CoreAndFood/WA_06_CoreAndFood/ViewComponents/GetFoodList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WA_06_CoreAndFood.ViewComponents
+{
+    public class GetFoodList
+    {
+    }
+}
